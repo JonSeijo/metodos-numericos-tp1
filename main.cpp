4d51ef6e7b8ad77d8c01383eb9ca921facc52e7f
jonseijo <jon.seijo@gmail.com>
2017-08-21 19:28:45 -0300

    Agrego clase Matriz y creo matriz de luces

--- main.cpp
+++ main.cpp
@@ -4,16 +4,47 @@ using std::string;
 using std::vector;
 using std::ifstream;
 
+string filepathLuces = "recursos/resultados_y_ejemplos/luces.txt";
+
 // Una luz seria un vector de 3 coordenadas
 typedef vector<double> luz;
 
-string filepathLuces = "recursos/resultados_y_ejemplos/luces.txt";
+class Matriz {
+    public:
+        vector<vector<double> > m;
+        int filas, columnas;
 
-int main() {
+    Matriz (int _filas, int _columnas) {
+        filas = _filas;
+        columnas = _columnas;
+        m.assign(filas, vector<double>(columnas, 0));
+    }
 
+    Matriz (vector<vector<double> > _m) {
+        m = _m;
+        filas = m.size();
+        columnas = filas != 0 ? m[0].size() : 0;
+    }
+
+    vector<double> &operator [] (int fila) {
+        return m[fila];
+    }
+
+    void print() {
+        std::cout << "Debug matriz: \n";
+        for (int f = 0; f < filas; f++) {
+            for (int c = 0; c < columnas; c++) {
+                std::cout << m[f][c] << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+};
+
+vector<luz> leerLuces() {
     vector<luz> luces(12);
 
-    // Leo el archivo de luces
     ifstream fileLuces;
     fileLuces.open(filepathLuces);
     if (fileLuces.is_open()) {
@@ -25,11 +56,26 @@ int main() {
             double x, y, z;
             fileLuces >> x >> y >> z;
             luces[i] = luz({x, y, z});
-
+            // @DEBUG
             std::cout << "luz: " << i << "     (" << x << ", " << y << ", " << z << ")\n";
         }
     }
 
+    return luces;
+}
 
 
+int main() {
+    // Leo el archivo de luces
+    vector<luz> luces = leerLuces();
+
+    // Ahora luces es un vector que tiene todas las luces
+
+    Matriz S({
+        luces[0],
+        luces[1],
+        luces[2]
+    });
+
+    S.print();
 }
