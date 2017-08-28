8d36f63f3379d1ab927fc2a0f8b040e3808e78ab
jonseijo <jon.seijo@gmail.com>
2017-08-27 21:50:02 -0300

    Empiezo algoritmo calibrador de luces

---
+++ CalibrarLuces.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "Imagen.cpp"
+
+using namespace std;
+
+/*
+Pasos a seguir:
+
+- Leer mascara de esfera y las 12 imagenes
+- Calcular R con la mascara
+- Calcular Cx, Cy con la mascara
+- Para cada imagen de esfera:
+    : identificar Px Py el punto mas iluminado
+    : hacer el calculo de vector luz para esta imagen
+
+*/
+
+int main() {
+
+
+
+}
--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ CPP=g++
 FLAGS= -std=c++11
 LIBS = Matriz.cpp Imagen.cpp recursos/ppmloader/ppmloader.cpp
 
-all: main
+all: main calibracion
 
 main: main.cpp $(LIBS)
 	$(CPP) $(FLAGS) -o $@ $(LIBS) $<
+
+calibracion: CalibrarLuces.cpp Imagen.cpp
+	$(CPP) $(FLAGS) -o $@ Imagen.cpp $<
 
 %.o: %.cpp
 	$(CPP) $(FLAGS) -c -o $@ $<
