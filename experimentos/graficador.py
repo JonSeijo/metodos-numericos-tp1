122895e5de0248874111c11f9b26504e2c25afa2
jonseijo <jon.seijo@gmail.com>
2017-09-07 11:24:29 -0300

    Agrego caratula y soluciono merge

--- experimentos/graficador.py
+++ experimentos/graficador.py
@@ -1,8 +1,8 @@
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 import matplotlib.pyplot as plt
 import pandas as pd
- 
- 
+
+
 gauss = []
 with open('gaussDim.txt') as my_file:
     for line in my_file:
@@ -34,33 +34,3 @@ plt.xlabel('dimension (n)')
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
            ncol=2, mode="expand", borderaxespad=0.)
 plt.show()
-'''
-# Creo un arreglo con las diferencias
-diff_zetas = [abs(luces_propias[i][2] - luces_catedra[i][2]) for i in range(12)]
- 
-# Tomo el maximo para graficar una linea
-maxdiffz = max(diff_zetas)
- 
-# Creo un dataframe con mis datos
-data = pd.DataFrame(diff_zetas)
-maximoz_data = pd.DataFrame([maxdiffz for _ in range(12)])
- 
- 
-# Plotteo las 12 muestras
-# plotter_ax = data.plot(style='.-', ylim=(0,1), xticks=list(range(12)))
-# NOTA: HACE .PLOT DIRECTO, LOS PARAMETROS SON PARA CUSTOMIZAR:
-plotter_ax = data.plot()
- 
-# Grafico una lina con mi maximo en el mismo plot
-maximoz_data.plot(ax=plotter_ax)
- 
-# plotter_ax.legend(["Diferencias", "Máxima diff"])
- 
-plotter_ax.set_title("Diferencia entre luces propias y de la cátedra, eje Z")
- 
-# plt.annotate('%0.2f' % maxdiffz, xy=(1, maxdiffz), xytext=(8, 0),
-#                  xycoords=('axes fraction', 'data'), textcoords='offset points')
- 
-plotter_ax.set_xlabel("Luz", size = 14)
-plotter_ax.set_ylabel("Diferencia en eje Z", size = 14)
-plt.show()'''
